5095edb186044f88c17f0e498eef614d60d8a447
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2025-05-23 14:38:30 -0300

    Revisão do contéudo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         serverSide: true,
         language: {
     
-            url: "http://cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json"
+            url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json"
     
         }
     
